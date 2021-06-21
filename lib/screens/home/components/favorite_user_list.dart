720c974b6a3e0af0453c131908d59990c4f244f1
rllyhz <rullyihza00@gmail.com>
2021-06-21 22:26:53 +0700

    Updating Favorite User widget

--- lib/screens/home/components/favorite_user_list.dart
+++ lib/screens/home/components/favorite_user_list.dart
@@ -1,3 +1,4 @@
+import 'package:chat_app/utils/data_utils.dart';
 import 'package:flutter/material.dart';
 
 class FavoriteUserList extends StatelessWidget {
@@ -5,8 +6,71 @@ class FavoriteUserList extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Container(
-      child: null,
+    return Padding(
+      padding: EdgeInsets.symmetric(vertical: 10.0),
+      child: Column(
+        children: <Widget>[
+          Padding(
+            padding: EdgeInsets.symmetric(horizontal: 20.0),
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: <Widget>[
+                Text(
+                  'Favorite Contacts',
+                  style: TextStyle(
+                    color: Colors.blueGrey,
+                    fontSize: 18.0,
+                    fontWeight: FontWeight.bold,
+                    letterSpacing: 1.0,
+                  ),
+                ),
+                IconButton(
+                  icon: Icon(
+                    Icons.more_horiz,
+                  ),
+                  iconSize: 30.0,
+                  color: Colors.blueGrey,
+                  onPressed: () {},
+                ),
+              ],
+            ),
+          ),
+          Container(
+            height: 120.0,
+            child: ListView.builder(
+              padding: EdgeInsets.only(left: 10.0),
+              scrollDirection: Axis.horizontal,
+              itemCount: favoriteUsers.length,
+              itemBuilder: (BuildContext context, int index) {
+                return InkWell(
+                  onTap: () {},
+                  child: Padding(
+                    padding: EdgeInsets.all(10.0),
+                    child: Column(
+                      children: <Widget>[
+                        CircleAvatar(
+                          radius: 35.0,
+                          backgroundImage:
+                              AssetImage(favoriteUsers[index].imageUrl),
+                        ),
+                        SizedBox(height: 6.0),
+                        Text(
+                          favoriteUsers[index].name,
+                          style: TextStyle(
+                            color: Colors.blueGrey,
+                            fontSize: 16.0,
+                            fontWeight: FontWeight.w600,
+                          ),
+                        ),
+                      ],
+                    ),
+                  ),
+                );
+              },
+            ),
+          ),
+        ],
+      ),
     );
   }
 }
