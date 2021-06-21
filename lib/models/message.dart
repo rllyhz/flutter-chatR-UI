6f28c604b013b54ad0527f8e28aec74ba4e9ec2b
rllyhz <rullyihza00@gmail.com>
2021-06-21 21:41:17 +0700

    Creating message model file

---
+++ lib/models/message.dart
@@ -0,0 +1,17 @@
+import 'package:chat_app/models/user.dart';
+
+class Message {
+  final User sender;
+  final String time;
+  final String text;
+  final bool isLiked;
+  final bool unread;
+
+  Message({
+    required this.sender,
+    required this.time,
+    required this.text,
+    required this.isLiked,
+    required this.unread,
+  });
+}
